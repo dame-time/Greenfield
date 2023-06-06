0f0925c941c227055b7c8950f593200ee0c38664
Davide Paollilo <davidepaollilo@MacBook-Pro-di-Davide.local>
2023-06-06 12:36:37 +0200

    Updated MVC pattern

---
+++ src/main/java/greenfield/controller/AdministrationClientController.java
@@ -0,0 +1,49 @@
+package greenfield.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import utils.data.StatisticsHTTPResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+public class AdministrationClientController {
+    private static final String serverURL = "http://localhost:8080/";
+
+    public List<String> getAllRobotIDs() throws IOException {
+        String endpointURL = serverURL + "robotsInfo/ids";
+        return new Gson().fromJson(getResponseFrom(endpointURL).toString(), new TypeToken<List<String>>(){}.getType());
+    }
+
+    public StatisticsHTTPResponse getRobotStats(String robotID, int n) throws IOException {
+        String endpointURL = serverURL + "robotsInfo/stats/" + robotID + "/" + n;
+        return new Gson().fromJson(getResponseFrom(endpointURL).toString(), StatisticsHTTPResponse.class);
+    }
+
+    public StatisticsHTTPResponse getRobotsStatsBetweenTimestamps(long t1, long t2) throws IOException {
+        String endpointURL = serverURL + "robotsInfo/stats/between/" + t1 + "/" + t2;
+        return new Gson().fromJson(getResponseFrom(endpointURL).toString(), StatisticsHTTPResponse.class);
+    }
+
+    private StringBuilder getResponseFrom(String endpointURL) throws IOException {
+        URL url = new URL(endpointURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+        String output;
+        StringBuilder sb = new StringBuilder();
+        while ((output = br.readLine()) != null) {
+            sb.append(output);
+        }
+
+        conn.disconnect();
+        return sb;
+    }
+}
--- src/main/java/greenfield/view/AdministrationClient.java
+++ src/main/java/greenfield/view/AdministrationClient.java
@@ -1,21 +1,13 @@
 package greenfield.view;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import greenfield.model.adminServer.AdministrationServer;
-import utils.data.StatisticsHTTPResponse;
+import greenfield.controller.AdministrationClientController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 import java.util.Scanner;
 
-// TODO: Create all this function into a class in the controller
 public class AdministrationClient {
-    private static final String serverURL = AdministrationServer.BASE_URI;
+    private static final AdministrationClientController controller = new AdministrationClientController();
 
     public static void main(String[] args) throws IOException {
         System.out.println("\033[1;33m" + "Welcome to the Administration Client! Please use the following commands:" + "\033[0m");
@@ -40,13 +32,13 @@ public class AdministrationClient {
             }
 
             if (parts.length == 1 && parts[0].equalsIgnoreCase("robots")) {
-                List<String> robotIDs = getAllRobotIDs();
+                List<String> robotIDs = controller.getAllRobotIDs();
                 System.out.println("\033[0;34m" + "Robot IDs: " + robotIDs + "\033[0m");
             } else if (parts.length == 2 && isInteger(parts[1])) {
-                var stats = getRobotStats(parts[0], Integer.parseInt(parts[1])).getAverageAirPollution();
+                var stats = controller.getRobotStats(parts[0], Integer.parseInt(parts[1])).getAverageAirPollution();
                 System.out.println("\033[0;35m" + "Stats for robot " + parts[0] + ": " + stats + "\033[0m");
             } else if (parts.length == 2 && isLong(parts[0]) && isLong(parts[1])) {
-                var stats = getRobotsStatsBetweenTimestamps(Long.parseLong(parts[0]), Long.parseLong(parts[1])).getAverageAirPollution();
+                var stats = controller.getRobotsStatsBetweenTimestamps(Long.parseLong(parts[0]), Long.parseLong(parts[1])).getAverageAirPollution();
                 System.out.println("\033[0;35m" + "Stats between timestamps " + parts[0] + " and " + parts[1] + ": " + stats + "\033[0m");
             } else {
                 System.out.println("\033[0;31m" + "Invalid command, please try again." + "\033[0m");
@@ -59,7 +51,7 @@ public class AdministrationClient {
             return false;
 
         try {
-            int value = Integer.parseInt(strNum);
+            Integer.parseInt(strNum);
         } catch (NumberFormatException nfe) {
             return false;
         }
@@ -72,48 +64,11 @@ public class AdministrationClient {
             return false;
 
         try {
-            long value = Long.parseLong(strNum);
+            Long.parseLong(strNum);
         } catch (NumberFormatException nfe) {
             return false;
         }
 
         return true;
-    }
-
-    private static List<String> getAllRobotIDs() throws IOException {
-        String endpointURL = serverURL + "robotsInfo/ids";
-
-        // This should be the way to correctly deserialize a List of strings
-        return new Gson().fromJson(getResponseFrom(endpointURL).toString(), new TypeToken<List<String>>(){}.getType());
-    }
-
-    private static StatisticsHTTPResponse getRobotStats(String robotID, int n) throws IOException {
-        String endpointURL = serverURL + "robotsInfo/stats/" + robotID + "/" + n;
-
-        return new Gson().fromJson(getResponseFrom(endpointURL).toString(), StatisticsHTTPResponse.class);
-    }
-
-    public static StatisticsHTTPResponse getRobotsStatsBetweenTimestamps(long t1, long t2) throws IOException {
-        String endpointURL = serverURL + "robotsInfo/stats/between/" + t1 + "/" + t2;
-
-        return new Gson().fromJson(getResponseFrom(endpointURL).toString(), StatisticsHTTPResponse.class);
-    }
-
-    private static StringBuilder getResponseFrom(String endpointURL) throws IOException {
-        URL url = new URL(endpointURL);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Accept", "application/json");
-
-        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-        String output;
-        StringBuilder sb = new StringBuilder();
-        while ((output = br.readLine()) != null) {
-            sb.append(output);
-        }
-
-        conn.disconnect();
-        return sb;
     }
 }
